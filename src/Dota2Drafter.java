e4c62c6a0adf8bccd2a7e1783a64b2c4a97f98e3
Jackson <jackson.bounds@gmail.com>
2022-06-19 15:45:19 -0400

    Initial commit

---
+++ src/Dota2Drafter.java
@@ -0,0 +1,10 @@
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+public class Dota2Drafter {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+
+    }
+}
