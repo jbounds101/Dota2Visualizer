495383a70d84d388602e0d7efa1c4f4f8cf8fb9a
Jackson <jackson.bounds@gmail.com>
2022-06-20 18:31:24 -0400

    Issues with modules

--- src/Dota2Drafter.java
+++ src/Dota2Drafter.java
@@ -1,10 +1,9 @@
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.HttpClients;
+import org.apache.*
 
 public class Dota2Drafter {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-
+        HttpGet g = new HttpGet("https");
+        //welp
     }
 }
