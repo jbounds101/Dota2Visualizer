f996110e77f5e1799a418959e3e25a9a35b43d0b
Jackson <jackson.bounds@gmail.com>
2022-07-08 15:36:15 -0400

    JavaFX is legit the best gui ever holy moly. Linked FXMLLoader to the fmxl file.

--- src/main/resources/Main.java
+++ src/main/resources/Main.java
@@ -1,14 +1,12 @@
 package main.resources;
 
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 
-import java.io.IOException;
 
 public class Main extends Application {
     /*  1. Stage (the window essentially)
@@ -23,18 +21,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Group root = new Group(); // Root node is how a scene is organized
-        Scene scene = new Scene(root, Color.BLACK); // Must pass a root node, arranged in different type based on root
-        stage.setTitle("This is the stage!");
-        Image icon = new Image("/javafx_images/icon.png"); // Image from javafx (don't include source,
-        // it assumes src is the starting dir)
-        stage.getIcons().add(icon); // Set icon at top left corner
-        stage.setWidth(400);
-        stage.setHeight(400);
-        stage.setResizable(false);
 
-
-        stage.setScene(scene); // Add scene to stage
+        Parent root = FXMLLoader.load(getClass().getResource("/main/resources/sample.fxml"));
+        stage.setTitle("Hello!");
+        stage.setScene(new Scene(root));
         stage.show();
     }
 }
