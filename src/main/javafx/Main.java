1fed05b0c3796f62728f3e0589ea9fc28a086e5a
Jackson <jackson.bounds@gmail.com>
2022-07-09 08:42:36 -0400

    Lambda for exit function in main

--- src/main/javafx/Main.java
+++ src/main/javafx/Main.java
@@ -1,9 +1,12 @@
 package main.javafx;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 
@@ -24,5 +27,25 @@ public class Main extends Application {
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+
+        stage.setOnCloseRequest(event -> {
+            event.consume();
+            logout(stage);
+        });
+
     }
+
+    public void logout(Stage stage) {
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+
+        alert.setTitle("Logout?");
+        alert.setHeaderText("You're about to logout!");
+        alert.setContentText("Do you want to save before exiting?");
+
+        if (alert.showAndWait().get() == ButtonType.OK) {
+            stage.close();
+        }
+    }
+
 }
