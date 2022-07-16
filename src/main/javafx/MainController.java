492c3714068d1e99822ca1dc00b26b897e3bc29a
Jackson <jackson.bounds@gmail.com>
2022-07-16 09:15:45 -0400

    Match search functionality nearly completed.

--- src/main/javafx/MainController.java
+++ src/main/javafx/MainController.java
@@ -1,15 +1,20 @@
 package main.javafx;
 
+import dotaobject.Match;
+import javafx.css.PseudoClass;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import main.DotaJsonParser;
+
 import java.io.IOException;
 
 
@@ -19,6 +24,10 @@ public class MainController {
     LineChart<Number, Number> lineChart;
     @FXML
     TextField searchBar;
+    @FXML
+    ImageView loadingIcon;
+    @FXML
+    ImageView searchIcon;
 
 
     private Stage stage;
@@ -26,7 +35,9 @@ public class MainController {
     private Parent root;
     private double windowWidth;
     private double windowHeight;
+    private Match match;
 
+    // #c177a8 : color of search icon
 
 
     public void labelButtonPress(MouseEvent event) throws IOException {
@@ -36,8 +47,23 @@ public class MainController {
 
     public void matchTextFieldSearch(KeyEvent event) throws IOException {
         if (event.getCode().equals(KeyCode.ENTER)) {
-            System.out.println("check");
+            loadingIcon.setVisible(true);
+            try {
+                long matchID = Long.parseLong(searchBar.getText());
+                new Thread(() -> {
+                    loadMatchDetails(matchID);
+                }).start();
+            } catch (NumberFormatException e) {
+                searchBar.setText("");
+            }
+
         }
     }
+
+    public void loadMatchDetails(Long matchID) {
+        match = DotaJsonParser.readMatch(matchID);
+        loadingIcon.setVisible(false);
+    }
+
 
 }
